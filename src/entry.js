ef74306aa62281be0f8c64a1cdb4c9d708d1f1a5
Eric Gehrman <eric.gehrman@projekt202.com>
2016-09-22 12:15:48 -0500

    added angular

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,8 @@
+var angular = require('angular'); 
+var angularRoute = require('angular-route');
+
 require('./sass/global.scss');
 require('font-awesome-webpack');
+
+
+window.app = angular.module([angularRoute]);
